f50869c0a98ef00ddd11325f7de3364a32ef22ba
Virginia Anderson <virginia.j.schmidt@gmail.com>
2022-05-13 14:57:38 -0400

    finding the best loand rates

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,14 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return (number * 2);
+    }
+
+    private static int add(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
     }
 }
