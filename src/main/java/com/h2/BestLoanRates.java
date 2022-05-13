1e2997b237d4f7fb53679b299f019b733be26979
Virginia Anderson <virginia.j.schmidt@gmail.com>
2022-05-13 14:58:44 -0400

    small typo fix

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class BestLoanRates {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+        System.out.println("Enter your name ");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
         System.out.println("Enter the loan term (in years) ");
