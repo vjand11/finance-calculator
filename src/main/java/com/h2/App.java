40721e81ed5059382eb7ce6797f37c3c6847dd5b
Virginia Anderson <virginia.j.schmidt@gmail.com>
2022-05-16 15:29:53 -0400

    minor change

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!" );
     }
 
     public static int doubleTheNumber(int number) {
