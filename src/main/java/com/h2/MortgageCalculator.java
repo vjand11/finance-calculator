b2705e3e721a37210417c1f936dc91d995ab9769
Virginia Anderson <virginia.j.schmidt@gmail.com>
2022-05-16 09:30:31 -0400

    module 4 updates

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,52 @@
+package com.h2;
+
+import java.text.DecimalFormat;
+
+public class MortgageCalculator {
+
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
+
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
+
+    private int getNumberOfPayments() {
+        return (termInYears * 12);
+    }
+
+    private float getMonthlyInterestRate() {
+        float interestRate = (annualRate / 100);
+        float monthlyRate = (interestRate / 12);
+        return monthlyRate;
+    }
+
+    public void calculateMonthlyPayment() {
+        long P = loanAmount;
+        float r = getMonthlyInterestRate();
+        int n = getNumberOfPayments();
+        double m = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
+        this.monthlyPayment = m;
+    }
+
+    public String toString() {
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
+    }
+
+    public static void main(String[] args) {
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
+
+        calculator.calculateMonthlyPayment();
+
+        System.out.println(calculator.toString());
+    }
+}
